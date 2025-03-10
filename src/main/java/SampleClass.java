66eed93c43ab3f22a7cae9b8aba29cdad7f5b6a1
RazaDon Willrich <razradical93@gmail.com>
2025-03-10 04:06:43 +0000

    Completed Parent Object Class Methods.

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -63,9 +63,17 @@ public class SampleClass {
     public boolean b;
 
     //    implement a custom .equals(SampleClass other){} method here.
+    public boolean equals(SampleClass other) {
+        if (other.a == this.a && other.b == this.b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 
     //    implement a custom .toString(){} method here.
-
-    
+    public String toString() {
+           return Integer.toString(a) + Boolean.toString(b);
+    }
 }
